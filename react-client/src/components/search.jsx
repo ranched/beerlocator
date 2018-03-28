c51c9d6cbf74b3d2b16e4eea0b5e7ab07932513c
Jason McLaughlin <jasonstephenm@gmail.com>
2018-03-27 21:54:53 -0500

    Clean up search function

--- react-client/src/components/search.jsx
+++ react-client/src/components/search.jsx
@@ -1,41 +1,32 @@
 import React from 'react';
 import { Form, FormControl, Button, Grid, Col } from 'react-bootstrap';
 
-const styles = {
-  Form: {
-    marginBottom: '30px'
-  }
-};
+const styles = { Form: { marginBottom: '30px' } };
 
 class Search extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = {
-      zipCode: '78701'
-    };
+    this.state = { zipCode: '78701' };
     this.onChange = this.onChange.bind(this);
     this.search = this.search.bind(this);
   }
 
   onChange(e) {
-    this.setState({
-      zipCode: e.target.value
-    });
+    this.setState({ zipCode: e.target.value });
   }
 
   search(api) {
     this.props.setLoad();
-    const url = process.env.NODE_ENV === 'production' ? `${process.env.IP}` : '127.0.0.1:3000';
-    console.log('url' + url);
+
     $.ajax({
-      method: "POST",
-      url: `http://${url}/breweries`,
-      data: { 
+      method: 'POST',
+      url: '/breweries',
+      data: {
         zipCode: this.state.zipCode,
-        api: 'yelp' 
-      }
-    }).done ( reply => {
-      console.log("searched with ", this.state.zipCode, " and reply is... ", typeof reply, reply);
+        api,
+      },
+    }).done((reply) => {
+      console.log('searched with ', this.state.zipCode, ' and reply is... ', typeof reply, reply);
       this.props.setBreweries(reply);
       this.props.setLoad();
     });
@@ -46,18 +37,18 @@ class Search extends React.Component {
             <Grid>
               <Col xs={12} md={8}>
                 <Form style={styles.Form} inline={true}>
-                  {/*<h4>Search for breweries!</h4>*/}
-                  <FormControl 
+                  {/* <h4>Search for breweries!</h4> */}
+                  <FormControl
                     className="Zipcode-input"
                     placeholder="Enter zipcode"
-                    value={this.state.terms} 
+                    value={this.state.terms}
                     onChange={this.onChange}
-                  />       
+                  />
                   <Button bsStyle="warning" onClick={this.search.bind(this, 'yelp')}> Search</Button>
                 </Form>
               </Col>
             </Grid>
-        )
+    );
   }
 }
 
