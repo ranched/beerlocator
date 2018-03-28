535a4cee51a7be072867eb34152f8404fb056b79
Jason McLaughlin <jasonstephenm@gmail.com>
2018-03-27 21:16:48 -0500

    Add airbnb lint config

--- helpers/checkAPIandSearch.js
+++ helpers/checkAPIandSearch.js
@@ -1,19 +1,16 @@
-var yelpFetch = require('./searchYelp.js');
+const yelpFetch = require('./searchYelp.js');
 
-
-function checkAPIandSearch(req){
-  if(req.body.api === 'yelp'){
-    return  yelpFetch.getBreweriesByLocation(req.body.zipCode)
-      .then(function(result){
-          breweries = JSON.parse(result).businesses;
-          breweries.forEach(item => { console.log('yelp results:', item.name);});
-          return breweries.map( brewery => brewery.name );
-
-       });
-  } else {
-    console.log('__________req.body:', req.body);
+function checkAPIandSearch(req) {
+  if (req.body.api === 'yelp') {
+    return yelpFetch.getBreweriesByLocation(req.body.zipCode)
+      .then((result) => {
+        const breweries = JSON.parse(result).businesses;
+        breweries.forEach(item => console.log('yelp results:', item.name));
+        return breweries.map(brewery => brewery.name);
+      });
   }
+  console.log('__________req.body:', req.body);
+  return req.body.api;
 }
 
 module.exports.checkAPIandSearch = checkAPIandSearch;
-
--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,19 +1,16 @@
-var models = require('../models/'),
-    db = require('../db/'),
-    checkAPIandSearch = require('../../helpers/checkAPIandSearch.js'),
-    responses = require('../../helpers/responses.js'),
-    getBreweryID = require('../../helpers/getBreweryID.js'),
-    searchBreweryID = require('../../helpers/searchBreweryID.js'),
-    fetchBreweryData = require('../../helpers/fetchBreweryData'),
-    fetchBeersByBrewery = require('../../helpers/fetchBeersByBrewery.js');
+const models = require('../models/');
+const checkAPIandSearch = require('../../helpers/checkAPIandSearch.js');
+const responses = require('../../helpers/responses.js');
+const fetchBreweryData = require('../../helpers/fetchBreweryData');
+// const fetchBeersByBrewery = require('../../helpers/fetchBeersByBrewery.js');
 
 module.exports.breweries = {
-  get: function (req, res) {
-    function sendResponse(res, data){
-      res.send({results: data});
+  get(req, res) {
+    function sendResponse(response, data) {
+      res.send({ results: data });
     }
     models.breweries.get(sendResponse.bind(null, res));
-  }, 
+  },
 
   // post: function (req, res) {
   //   var data = req.body;
@@ -21,80 +18,47 @@ module.exports.breweries = {
   //     res.send();
   //   }
   //   models.breweries.post(data, sendResponse.bind(null, res));
-
-
   // }
 
-  post: function(req, res){
+  post(req, res) {
     console.log('POST REQUEST RECEIVED');
 
-    let sendResponse = responses.sendPostResponse.bind(null, res);
+    const sendResponse = responses.sendPostResponse.bind(null, res);
 
-    let breweries = [];
-      
-    checkAPIandSearch.checkAPIandSearch(req)  
-    // .then(function(breweries){
-    
-    //   let promises = [];
-    //   breweries.forEach(brewery => {
-    //     promises.push(searchBreweryID.searchBreweryIDbreweryDB(brewery));
-    //   });
-    //   return Promise.all(promises);
-    
-    // })
-    // .then(function(breweries){ 
-    
-    //   let names = [];
-    //   for(let i = 0; i < breweries.length; i++){
-    //     if (breweries[i]){
-    //     names.push(breweries[i].replace(/Company/g, ''));
-    //     }
-    //   }
-    //   console.log('Breweries after BID search in BDB:');
-    //   names.forEach(item => console.log('name:', item));
-    //   return names;
-    // })
-    .then(function(names){
+    checkAPIandSearch.checkAPIandSearch(req)
+      .then((names) => {
+        const promises = [];
 
-      let promises = [];
+        names.forEach((name) => {
+          promises.push(fetchBreweryData.brewerySearchBA(name)
+            .then(fetchBreweryData.scrapeBrewerySearchBA));
+        });
 
-      const log = (data) => {
-        
-        console.log('data:', data);
-        return data;
-      };
+        return Promise.all(promises);
+      })
 
-      names.forEach(name => {
-        promises.push(fetchBreweryData.brewerySearchBA(name).then(fetchBreweryData.scrapeBrewerySearchBA));
+      .then((breweryObjs) => {
+        console.log('breweries to get scores for: ', breweryObjs);
+        const promises = [];
+
+        breweryObjs.forEach((obj) => {
+          console.log('breweryObj: ', obj);
+          if (obj[0].brewery_url !== undefined) {
+            promises.push(fetchBreweryData.breweryPageBA(obj[0].brewery_url)
+              .then(fetchBreweryData.scrapeBreweryPageBA));
+          }
+        });
+        return Promise.all(promises);
+      })
+
+      .then((breweryObjs) => {
+        console.log('response: ', breweryObjs);
+        sendResponse(breweryObjs);
+      })
+
+      .catch((err) => {
+        sendResponse();
+        return console.log(err);
       });
-
-
-      return Promise.all(promises);
-
-    })
-    .then(function(breweryObjs){
-      console.log('breweries to get scores for: ', breweryObjs);
-      let promises = [];
-
-      breweryObjs.forEach((obj, i) => {
-        console.log('breweryObj: ', obj);
-        if(obj[0].brewery_url !== undefined){
-          promises.push(fetchBreweryData.breweryPageBA(obj[0].brewery_url).then(fetchBreweryData.scrapeBreweryPageBA));
-        }
-      });
-      return Promise.all(promises);
-      
-    })
-    .then(function(breweryObjs){
-      console.log('response: ', breweryObjs);
-      sendResponse(breweryObjs);
-    })
-    .catch(function(err){
-    
-      sendResponse();
-      return console.log(err);
-    
-    });
-
-  }
+  },
 };
